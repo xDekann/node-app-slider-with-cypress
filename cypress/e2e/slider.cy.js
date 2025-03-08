e844479e3ace44f76a4d70b7e252ea22c68e831d
Oskar Ożóg <106389146+xDekann@users.noreply.github.com>
2025-03-08 18:19:10 +0100

    Hotjar + e2e

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,29 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper back and forth', function () {
+  it('Checks if navigation allows moving back and forth', function () {
+    cy.visit('http://localhost:3000');
+    
+    // Move from Rome to London
+    cy.get('.swiper-button-next').click();
+    cy.wait(1000);
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    
+    // Move from London to Paris
+    cy.get('.swiper-button-next').click();
+    cy.wait(1000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    
+    // Move back to London
+    cy.get('.swiper-button-prev').click();
+    cy.wait(1000);
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    
+    // Move back to Rome
+    cy.get('.swiper-button-prev').click();
+    cy.wait(1000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
